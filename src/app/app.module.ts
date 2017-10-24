cae2ab363da0bcc201972e251a82c7c79045e96d
Arne Schubert <atd.schubert@gmail.com>
2017-10-24 14:09:14 +0200

    Register modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { YagaModule } from '@yaga/leaflet-ng2';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    YagaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
